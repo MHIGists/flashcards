c266b2f2efd666e53b0d158d18297fd92c1466f4
Milko <milko.ivanov@ctgaming.com>
2024-09-10 17:46:01 +0300

    Add welcome screen. TODO work on the responsive design & SEO

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function (){
-    return redirect()->route('login');
-});
+//Route::get('/', function (){
+//    return redirect()->route('login');
+//});
+
+Route::view('/welcome', 'welcome')->name('welcome');
+Route::view('/', 'welcome');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
