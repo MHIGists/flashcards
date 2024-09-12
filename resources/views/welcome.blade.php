b2277c4e8831ae18827b36cf235a771ebd05535f
Milko <milko.ivanov@ctgaming.com>
2024-09-12 15:18:40 +0300

    Add canoncial  href for pages. Have to change keywords and description for all public pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <meta charset="utf-8">
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link rel="canonical" href="{{url()->current()}}" />
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="description" content="Discover free flashcards for self-study or teaching kids. Create flashcard groups for easy management. Everything is available free of charge!">
         <meta name="keywords" content="your ,flashcards, flash, cards, study, teach, learning, groups, free, progress, organize, prepare, user, subjects, topics, focused, exam, manage, private, multiple, register, welcome, start, improve, stay updated">
 
